3dc8d4dd457f5cb10987ee05f9d3416215efa75b
sceiiya <meetscheidj@gmail.com>
2025-02-19 23:52:09 +0800

    debug: update

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -10,8 +10,37 @@ contract CounterScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.startBroadcast();
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        address acc = vm.addr(deployerPrivateKey);
 
+        console.log("Deployer address: ", acc);
+
+        vm.startBroadcast();
+        // address deployerAddress = vm.addr(deployerPrivateKey);
+
+        // vm.startBroadcast(deployerPrivateKey);
+
+        // uint64 nonce = vm.getNonce(deployerAddress);
+
+        // console.log("nonce: ", nonce);
+        // vm.setNonce(deployerAddress, ++nonce);
+
+        // counter = new Counter();
+        // console.log("Deploying contract...");
+        // counter.deploy();
+        // console.log("Deployed contract at: ", counter.address());
+
+        // Set custom gas price (in wei)
+        // uint256 gasPrice = 20000000000; // 20 gwei (customize this as needed)
+
+        // Start broadcasting with the custom gas price
+        // vm.startBroadcast(deployerPrivateKey, gasPrice);
+        vm.startBroadcast(deployerPrivateKey);
+
+        // uint64 nonce = vm.getNonce(deployerAddress);
+        // console.log("nonce: ", nonce);
+
+        // Deploy the contract
         counter = new Counter();
 
         vm.stopBroadcast();
